b0138ec9924bee7833069d971b9240ad9a5d9a24
Marina Blin <marina.blin@free.fr>
2019-02-12 19:06:59 +0100

    :sparkles: All pokemon from same type

--- src/components/Pokedex/pokedex.jsx
+++ src/components/Pokedex/pokedex.jsx
@@ -1,50 +1,75 @@
-import React from 'react';
+import React from 'react'
 import * as api from '../../api'
-import {PokemonForPokedex} from '../PokemonForPokedex/pokemonForPokedex'
-import styles from './pokedex.module.css';
+import { PokemonForPokedex } from '../PokemonForPokedex/pokemonForPokedex'
+import styles from './pokedex.module.css'
 
 export class Pokedex extends React.Component {
-    state = {allPokemons: null}
+  state = {
+    allPokemons: null,
+  }
 
-    componentDidMount() {
-        api.getPokemons()
-            .then(response => response.json())
-            .then(data => this.setState({allPokemons: data}))
+  componentDidMount() {
+    console.log('this.props.allPokemons : ', this.props.allPokemons)
+    if (this.props.allPokemons) {
+      console.log('passage dans le if')
+      this.setState({ allPokemons: this.props.allPokemons })
+    } else {
+      api
+        .getPokemons()
+        .then(response => response.json())
+        .then(data => this.setState({ allPokemons: data }))
     }
+    console.log('this.state : ', this.state)
+  }
 
-    onNext = () => {
-        if (!this.state.allPokemons || !this.state.allPokemons.next) return
-        return fetch(this.state.allPokemons.next)
-                .then(response => response.json())
-                .then(data => this.setState({allPokemons: data}))
-    }
+  onNext = () => {
+    if (!this.state.allPokemons || !this.state.allPokemons.next) return
+    return fetch(this.state.allPokemons.next)
+      .then(response => response.json())
+      .then(data => this.setState({ allPokemons: data }))
+  }
 
-    onPrevious = () => {
-        if (!this.state.allPokemons || !this.state.allPokemons.previous) return
-        return fetch(this.state.allPokemons.previous)
-                .then(response => response.json())
-                .then(data => this.setState({allPokemons: data}))
-    }
+  onPrevious = () => {
+    if (!this.state.allPokemons || !this.state.allPokemons.previous) return
+    return fetch(this.state.allPokemons.previous)
+      .then(response => response.json())
+      .then(data => this.setState({ allPokemons: data }))
+  }
 
-    render() {
-        const {allPokemons} = this.state
-        const {count, next, previous, results = []} = allPokemons || {}
+  render() {
+    const { allPokemons } = this.state
 
-        if (!allPokemons) return <div>Chargement en cours</div>
+    const { count = null, next = null, previous = null, results = [] } = allPokemons || {}
 
-        const list = results.map(({name, url}) => <PokemonForPokedex key={name} url={url} name={name}/>)
+    if (!results) return <div>Chargement en cours</div>
 
-        return <div className={styles.fullHeight}>
-            <h1 className={styles.centerElement}>Pokedex !</h1>
-            <div className={styles.centerElement}>
-                <button disabled={!previous} onClick={this.onPrevious}>previous</button>
-                <button disabled={!next} onClick={this.onNext}>next</button>
-            </div>
-            <div className={styles.pokedex}>{list}</div>
-            <div className={styles.centerElement}>
-                <button disabled={!previous} onClick={this.onPrevious}>previous</button>
-                <button disabled={!next} onClick={this.onNext}>next</button>
-            </div>
+    const list = results.samePokemon
+      ? results.samePokemon.map(({ name, url }) => (
+          <PokemonForPokedex key={name} url={url} name={name} />
+        ))
+      : results.map(({ name, url }) => <PokemonForPokedex key={name} url={url} name={name} />)
+
+    return (
+      <div className={styles.fullHeight}>
+        <h1 className={styles.centerElement}>Pokedex ! ({count || 0} pokemons)</h1>
+        <div className={styles.centerElement}>
+          <button disabled={!previous} onClick={this.onPrevious}>
+            previous
+          </button>
+          <button disabled={!next} onClick={this.onNext}>
+            next
+          </button>
         </div>
-    }
+        <div className={styles.pokedex}>{list}</div>
+        <div className={styles.centerElement}>
+          <button disabled={!previous} onClick={this.onPrevious}>
+            previous
+          </button>
+          <button disabled={!next} onClick={this.onNext}>
+            next
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
--- src/components/Type/type.jsx
+++ src/components/Type/type.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
-import * as api from '../../api';
-import { PokemonForPokedex } from '../PokemonForPokedex/pokemonForPokedex';
+import React from 'react'
+import * as api from '../../api'
+import { Pokedex } from '../Pokedex/pokedex'
 
 export class Type extends React.Component {
-    state = { type: null }
+  state = { type: null }
 
-    componentDidMount() {
-        api.getType(this.props.name)
-            .then(response => response.json())
-            .then(data => this.setState({ type: data }))
-    }
+  componentDidMount() {
+    api
+      .getType(this.props.name)
+      .then(response => response.json())
+      .then(data => this.setState({ type: data }))
+  }
 
-    render() {
-        if (!this.state.type) return <p>Chargement en cours</p>
+  render() {
+    if (!this.state.type) return <p>Chargement en cours</p>
 
-        console.log(this.state.type.pokemon)
-        const samePokemon = this.state.type.pokemon.map(({pokemon: {name, url}}) => {
-            return <PokemonForPokedex
-                key={name}
-                url={url}
-                name={name} />
-        })
-        return <div>
-            {samePokemon}
-        </div>
-    }
+    console.log(this.state.type.pokemon)
+    const samePokemon = this.state.type.pokemon.map(({ pokemon: { name, url } }) => {
+      return { name, url }
+    })
+    return (
+      <Pokedex
+        allPokemons={{
+          count: samePokemon.length,
+          next: null,
+          previous: null,
+          results: { samePokemon },
+        }}
+      />
+    )
+  }
 }
