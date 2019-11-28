13e89b6005517b230cbaaa181d4e69025bccd1d3
Tarek Touati <tarek.touati@zenika.com>
2019-11-28 17:03:44 +0100

    added test specs for search bar and pdf download (#7)
    * added cypress test specs for search bar and pdf download

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,13 +1,40 @@
-describe('The Home Page',  () => {
-    it('successfully loads',() => cy.visit('http://localhost:3000') );
-    it('previous button should be disabled', function() {
-        cy.contains('previous').should('be.disabled');
-    });
-    it('previous button should be enaabled',() => {
-        cy.contains('next').click();
-        cy.contains('previous').should('not.be.disabled');
-    });
-    it('should display all pokemons',() => {
-         cy.get('a').its('length').should('not.eq', 0)
-    });
-  })
+describe("The Home Page", () => {
+  it("successfully loads", () => cy.visit("http://localhost:3000"));
+  it("previous button should be disabled", function() {
+    cy.contains("previous").should("be.disabled");
+  });
+  it("previous button should be enaabled", () => {
+    cy.contains("next").click();
+    cy.contains("previous").should("not.be.disabled");
+  });
+  it("should display all pokemons", () => {
+    cy.get("a")
+      .its("length")
+      .should("not.eq", 0);
+  });
+});
+
+describe("The pokemon infos page", () => {
+  it("successfully loads", () => cy.visit("http://localhost:3000"));
+  it("should navigate to pokemon infos page", () => {
+    cy.get("a")
+      .eq(0)
+      .click();
+  });
+  it("should download pokemon pdf", () => {
+    cy.contains("Download PDF").click();
+  });
+});
+
+describe("Watch ", () => {
+  it("successfully loads", () => cy.visit("http://localhost:3000"));
+  it("should display pokemons based on type given ", () => {
+    cy.get("input").type("bug");
+    //wait 500 to fetch request ends
+    //TODO : find another way to wait for request to end
+    cy.wait(800);
+    cy.get("a")
+      .its("length")
+      .should("eq", 86);
+  });
+});
--- src/components/Pokedex/pokedex.jsx
+++ src/components/Pokedex/pokedex.jsx
@@ -1,57 +1,82 @@
-import React from 'react'
-import * as api from '../../api'
-import { PokemonForPokedex } from '../PokemonForPokedex/pokemonForPokedex'
-import styles from './pokedex.module.css'
+import React from "react";
+import * as api from "../../api";
+import { PokemonForPokedex } from "../PokemonForPokedex/pokemonForPokedex";
+import styles from "./pokedex.module.css";
 
 export class Pokedex extends React.Component {
   state = {
-    allPokemons: null,
-  }
+    allPokemons: null
+  };
 
   componentDidMount() {
-    console.log('this.props.allPokemons : ', this.props.allPokemons)
+    console.log("this.props.allPokemons : ", this.props.allPokemons);
     if (this.props.allPokemons) {
-      console.log('passage dans le if')
-      this.setState({ allPokemons: this.props.allPokemons })
+      console.log("passage dans le if");
+      this.setState({ allPokemons: this.props.allPokemons });
     } else {
       api
         .getPokemons()
         .then(response => response.json())
-        .then(data => this.setState({ allPokemons: data }))
+        .then(data => this.setState({ allPokemons: data }));
     }
-    console.log('this.state : ', this.state)
+    console.log("this.state : ", this.state);
   }
 
   onNext = () => {
-    if (!this.state.allPokemons || !this.state.allPokemons.next) return
+    if (!this.state.allPokemons || !this.state.allPokemons.next) return;
     return fetch(this.state.allPokemons.next)
       .then(response => response.json())
-      .then(data => this.setState({ allPokemons: data }))
-  }
+      .then(data => this.setState({ allPokemons: data }));
+  };
 
   onPrevious = () => {
-    if (!this.state.allPokemons || !this.state.allPokemons.previous) return
+    if (!this.state.allPokemons || !this.state.allPokemons.previous) return;
     return fetch(this.state.allPokemons.previous)
       .then(response => response.json())
-      .then(data => this.setState({ allPokemons: data }))
-  }
+      .then(data => this.setState({ allPokemons: data }));
+  };
+  // findPokemon return Array of pokemon based on given type
+  findPokemon = ({ target }) =>
+    api
+      .getType(target.value)
+      .then(resp =>
+        resp.json().then(({ pokemon }) => {
+          const newState = {
+            allPokemons: {
+              count: (pokemon && pokemon.length) || 0,
+              next: null,
+              previous: null,
+              results: {
+                samePokemon: pokemon.map(({ pokemon }) => pokemon)
+              }
+            }
+          };
+          this.setState(newState);
+        })
+      )
+      .catch(e => console.error(e));
 
   render() {
-    const { allPokemons } = this.state
+    const { allPokemons } = this.state;
 
-    const { count = null, next = null, previous = null, results = [] } = allPokemons || {}
+    const { count = null, next = null, previous = null, results = [] } =
+      allPokemons || {};
 
-    if (!results) return <div>Chargement en cours</div>
+    if (!results) return <div>Chargement en cours</div>;
 
     const list = results.samePokemon
       ? results.samePokemon.map(({ name, url }) => (
           <PokemonForPokedex key={name} url={url} name={name} />
         ))
-      : results.map(({ name, url }) => <PokemonForPokedex key={name} url={url} name={name} />)
+      : results.map(({ name, url }) => (
+          <PokemonForPokedex key={name} url={url} name={name} />
+        ));
 
     return (
       <div className={styles.fullHeight}>
-        <h1 className={styles.centerElement}>Pokedex ! ({count || 0} pokemons)</h1>
+        <h1 className={styles.centerElement}>
+          Pokedex ! ({count || 0} pokemons)
+        </h1>
         <div className={styles.centerElement}>
           <button disabled={!previous} onClick={this.onPrevious}>
             previous
@@ -60,6 +85,7 @@ export class Pokedex extends React.Component {
             next
           </button>
         </div>
+        <input type="text" onChange={this.findPokemon} />
         <div className={styles.pokedex}>{list}</div>
         <div className={styles.centerElement}>
           <button disabled={!previous} onClick={this.onPrevious}>
@@ -70,6 +96,6 @@ export class Pokedex extends React.Component {
           </button>
         </div>
       </div>
-    )
+    );
   }
 }
